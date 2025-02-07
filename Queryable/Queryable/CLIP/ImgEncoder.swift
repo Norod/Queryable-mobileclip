46a8483d92d69128d09cb0f41b80f27f339e2651
Doron Adler <doron@glidetalk.com>
2025-02-07 21:07:21 +0200

    Looks like there is a memory leak and with a large amount of images

--- Queryable/Queryable/CLIP/ImgEncoder.swift
+++ Queryable/Queryable/CLIP/ImgEncoder.swift
@@ -34,29 +34,30 @@ public struct ImgEncoder {
     let queue = DispatchQueue(label: "imgencoder.predict")
     
     private func encode(image: UIImage) async throws -> MLShapedArray<Float32> {
-        do {
-            guard let resizedImage = try image.resizeImageTo(size:CGSize(width: 256, height: 256)) else {
-                throw ImageEncodingError.resizeError
+        try autoreleasepool {
+            do {
+                guard let resizedImage = try image.resizeImageTo(size:CGSize(width: 256, height: 256)) else {
+                    throw ImageEncodingError.resizeError
+                }
+                
+                guard let buffer = resizedImage.convertToBuffer() else {
+                    throw ImageEncodingError.bufferConversionError
+                }
+                
+                guard let inputFeatures = try? MLDictionaryFeatureProvider(dictionary: ["colorImage": buffer]) else {
+                    throw ImageEncodingError.featureProviderError
+                }
+                
+                let result = queue.sync { autoreleasepool { try? model.prediction(from: inputFeatures) }}
+                guard let embeddingFeature = result?.featureValue(for: "embOutput"),
+                      let multiArray = embeddingFeature.multiArrayValue else {
+                    throw ImageEncodingError.predictionError
+                }
+                return MLShapedArray<Float32>(converting: multiArray)
+            } catch {
+                print("Error in encoding: \(error)")
+                throw error
             }
-            
-            guard let buffer = resizedImage.convertToBuffer() else {
-                throw ImageEncodingError.bufferConversionError
-            }
-            
-            guard let inputFeatures = try? MLDictionaryFeatureProvider(dictionary: ["colorImage": buffer]) else {
-                throw ImageEncodingError.featureProviderError
-            }
-            
-            let result = try queue.sync { try model.prediction(from: inputFeatures) }
-            guard let embeddingFeature = result.featureValue(for: "embOutput"),
-                  let multiArray = embeddingFeature.multiArrayValue else {
-                throw ImageEncodingError.predictionError
-            }
-            
-            return MLShapedArray<Float32>(converting: multiArray)
-        } catch {
-            print("Error in encoding: \(error)")
-            throw error
         }
     }
 }
